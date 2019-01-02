6f632da7f7addc48817e1502833216826a694d85
4enzo <archer.enzo@hotmail.com>
2019-01-02 21:15:29 +0800

    结果写入mongodbb数据库

--- one.py
+++ one.py
@@ -10,6 +10,7 @@ import requests
 import bs4
 import logging
 import time
+import pymongo
 
 '''
 爬取one:http://wufazhuce.com/
@@ -35,16 +36,30 @@ def spider_one(num):
 
             with open('one.txt','a',encoding='utf-8') as f:
                 f.write(title +'\n'+ content +'\n')
-            print('Success %s'%full_url)
+            write2db(title,content)
+            print('Success %s' % full_url)
             # time.sleep(5)
 
         except Exception as e:
             print('Failed:%s'%full_url)
-            logging.error('Error:%s'%(full_url))
-            logging.error(e)
+            print(e)
+            logging.error('Error:%s%s'%(full_url,e))
+            # logging.error(e)
             continue
     end_time = time.time()
     print('time spend %ss'%(end_time-start_time))
 
+def write2db(title,content):
+
+    conn = pymongo.MongoClient('192.168.199.170',27017)
+    # 连接spider数据库，没有则自动创建
+    db = conn.spider
+    # 使用one集合，没有则自动创建
+    my_collection = db.one
+    my_collection.insert({'title':title,'comments':content})
+
+    conn.close()
+
+
 if __name__ == '__main__':
-    spider_one(2294)
+    spider_one(2279)
